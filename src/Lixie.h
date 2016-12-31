e4274e4773c8ef96f1a6fd67e8dcc85c2e496d34
Connor Nishijima <connornishijima@gmail.com>
2016-12-31 15:03:40 -0700

    Update Lixie.h

--- src/Lixie.h
+++ src/Lixie.h
@@ -1,7 +1,7 @@
 /*
-  Lixie.h - Library for driving Lixie displays!
-  Created by Connor Nishijima, October 26th 2016.
-  Released under the GPLv3 license.
+Lixie.h - Library for driving Lixie displays!
+Created by Connor Nishijima, October 26th 2016.
+Released under the GPLv3 license.
 */
 
 #ifndef lixie_h
@@ -12,35 +12,35 @@
 
 #define write_string(input) write_string_f(input, sizeof(input)-1);
 
-class Lixie
-{
-  public:
-    Lixie();
-    void begin();
-	void clear();
+class Lixie{
+	public:
+		Lixie();
+		void begin();
+		void clear();
 
-	void write_int(float input);
-	void push_digit(byte number);
-	void write_digit(byte input, byte index);
+		void write_int(float input);
+		void push_digit(byte number);
+		void write_digit(byte input, byte index);
 
-	void write_string_f(char* input, byte len);
-	void write_char(char input, byte index);
+		void write_string_f(char* input, byte len);
+		void write_char(char input, byte index);
 
-	void show();
-	void print_binary();
-  uint8_t get_numdigits() const;
-	
-	void color_on(byte r, byte g, byte b);
-	void color_on(CRGB c);
-	
-	void color_on(byte r, byte g, byte b, byte index);
-	void color_on(CRGB c, byte index);
-	
-	void color_off(byte r, byte g, byte b);
-	void color_off(CRGB c);
-	
-	void color_off(byte r, byte g, byte b, byte index);
-	void color_off(CRGB c, byte index);
+		void show();
+		void print_binary();
+		uint8_t get_numdigits() const;
+		bool maxed_out(float input);
+
+		void color_on(byte r, byte g, byte b);
+		void color_on(CRGB c);
+
+		void color_on(byte r, byte g, byte b, byte index);
+		void color_on(CRGB c, byte index);
+
+		void color_off(byte r, byte g, byte b);
+		void color_off(CRGB c);
+
+		void color_off(byte r, byte g, byte b, byte index);
+		void color_off(CRGB c, byte index);
 };
 
 #endif
