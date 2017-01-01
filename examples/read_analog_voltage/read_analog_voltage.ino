0c1858cb5e37f5368ee8373ac6ac1ecaefc1fbb2
Connor Nishijima <connornishijima@gmail.com>
2017-01-01 05:05:26 -0700

    Create read_analog_voltage.ino

---
+++ examples/read_analog_voltage/read_analog_voltage.ino
@@ -0,0 +1,13 @@
+#include "Lixie.h" // Include Lixie Library
+Lixie lix;         // Set class nickname for faster coding
+
+void setup() {
+  lix.begin(); // Initialize LEDs
+}
+
+void loop() {
+  byte reading = analogRead(A0) / 4; // Map 10-bit to 8-bit
+  lix.color_on(255-reading,reading,0);
+  lix.write((reading/255.0)*500); // Display reading as 0 - 5(.)00
+  delay(100);
+}
