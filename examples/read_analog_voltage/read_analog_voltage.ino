19d96a34b12c66d6080b994abf7c308fc5943b13
Connor Nishijima <connornishijima@gmail.com>
2017-01-04 03:42:41 -0700

    Update read_analog_voltage.ino

--- examples/read_analog_voltage/read_analog_voltage.ino
+++ examples/read_analog_voltage/read_analog_voltage.ino
@@ -7,7 +7,7 @@ void setup() {
 
 void loop() {
   byte reading = analogRead(A0) / 4; // Map 10-bit to 8-bit
-  lix.color_on(255-reading,reading,0);
+  lix.color(255-reading,reading,0);
   lix.write((reading/255.0)*500); // Display reading as 0 - 5(.)00
   delay(100);
 }
