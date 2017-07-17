20da413532f55e1e95c972f011eaf6b8280568f5
Connor Nishijima <connornishijima@gmail.com>
2017-07-17 01:42:52 -0600

    Update Lixie.h

--- src/Lixie.h
+++ src/Lixie.h
@@ -8,7 +8,13 @@ Released under the GPLv3 license.
 #define lixie_h
 
 #include "Arduino.h"
-#define FASTLED_ESP8266_RAW_PIN_ORDER
+
+#ifdef ESP8266
+	#define FASTLED_ESP8266_RAW_PIN_ORDER
+	#define FASTLED_ALLOW_INTERRUPTS 0
+	#define FASTLED_INTERRUPT_RETRY_COUNT 0
+#endif
+
 #include "FastLED.h"
 
 #ifndef LED_TYPE
@@ -25,19 +31,21 @@ class Lixie{
 		void begin();
    
 		void clear(bool show_change = true);
+		void clear_digit(byte index, bool show_change = true);
 		void show();
 		
 		void write(uint32_t input);
 		void write(char* input);
 		
-		void write_flip(uint32_t input, uint16_t flip_time = 100);
+		void write_flip(uint32_t input, uint16_t flip_time = 100, uint8_t flip_speed = 10);
+		void write_fade(uint32_t input, uint16_t fade_time = 250);
 		
-		void sweep(CRGB col);
-		void progress(byte percent, CRGB col1, CRGB col2);
-		void flash_in(CRGB col);
-		void flash_out(CRGB col);
+		void sweep(CRGB col, byte speed = 15);
+		void progress(float percent, CRGB col1, CRGB col2);
+		void fill_fade_in(CRGB col, byte fade_speed = 20);
+		void fill_fade_out(CRGB col, byte fade_speed = 20);
     
-		void write_digit(byte input, byte index);
+		void write_digit(byte input, byte index, bool show_change = true);
 		void push_digit(byte number);
     
 		void color(byte r, byte g, byte b);
@@ -55,9 +63,16 @@ class Lixie{
 		
 		void color_array_fade(CRGB *cols, uint16_t duration);
 		void color_array_fade(CHSV *cols, uint16_t duration);
+		
+		void color_wipe(CRGB col1, CRGB col2);
 
+		void nixie_mode(bool enabled, bool has_aura = true);
+		void nixie_aura_intensity(byte val);
+		
 		void brightness(byte bright);
-		void color_balance(CRGB c_adj);
+		void white_balance(CRGB c_adj);
+		
+		void rainbow(uint8_t r_hue, uint8_t r_sep);
 
 		void max_power(byte volts, uint16_t milliamps);
 	
@@ -69,6 +84,7 @@ class Lixie{
 		// Debug Functions
 		void print_binary() const;
 		void print_current() const;
+		uint32_t get_number();
 
 	private:
 		static constexpr byte Addresses[10] = {3, 4, 2, 0, 8, 6, 5, 7, 9, 1};
@@ -79,15 +95,26 @@ class Lixie{
 		CRGB *colors;
 		CRGB *colors_off;
 		byte *led_states;
+		byte *digit_brightness;
 		CLEDController *controller;
-		byte bright = 255;
-		byte sweep_dir = 0;
+		byte sweep_dir = 1;
 		void setBit(uint16_t pos, byte val);
 		byte getBit(uint16_t pos) const;
 		byte get_size(uint32_t input) const;
 		byte char_to_number(char input) const;
 		bool char_is_number(char input) const;
 		void build_controller(uint8_t DataPin);
+		uint32_t current_number = 0;
+		byte current_number_size = 0;
+		byte current_number_arr[20];
+		void store_current(uint32_t input);
+		
+		bool nixie = false;
+		bool nixie_aura = false;
+		byte nixie_aura_level = 0;
+		byte nixie_aura_level_default = 8;
+		CRGB nixie_col_ion = CRGB(255,70,25);
+		CRGB nixie_col_aura = CRGB(45,85,255);
 };
 
 #endif
